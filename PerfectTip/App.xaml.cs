ad5cce23c91aab8ebe396b5c9f44978a1d832111
ethelpoe <macias.itnl@gmail.com>
2022-11-07 19:25:37 -0600

    Add project files.

---
+++ PerfectTip/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace PerfectTip;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
---
+++ PerfectTip/MainPage.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PerfectTip;
+
+public partial class MainPage : ContentPage
+{
+	decimal bill;
+	int tip;
+	int noPerson = 1;
+
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void txtBill_Completed(object sender, EventArgs e)
+	{
+		bill = decimal.Parse(txtBill.Text);
+		CalculateTotal();
+
+    }
+
+	private void CalculateTotal()
+	{
+		//total tip
+		var totalTip = (bill * tip) / 100;
+
+		//tip by person
+		var tipByPerson = (totalTip / noPerson);
+		//lblTip.Text = tipByPerson.ToString();
+
+		//****Concurrency format****
+		lblTip.Text = $"{tipByPerson:C}";
+
+		//subtotal
+		var subTotal = (bill / noPerson);
+        //lblSubtotal.Text = subTotal.ToString();
+        lblSubtotal.Text = $"{subTotal:C}";
+
+        //total
+        var totalByPerson = (bill + totalTip) / noPerson;
+        //lblTotal.Text = totalByPerson.ToString();
+        lblTotal.Text = $"{totalByPerson:C}";
+
+    }
+	//what is a object sender??
+	private void sldTip_ValueChanged(object sender, ValueChangedEventArgs e)
+	{
+		tip = (int)sldTip.Value;
+		lblTips.Text = $"Tip: {tip}%";
+		CalculateTotal();
+	}
+
+	private void Button_Clicked(object sender, EventArgs e)
+	{
+		if(sender is Button)
+		{
+			var btn = (Button)sender;
+			var percentage = int.Parse(btn.Text.Replace("%",""));
+			sldTip.Value = percentage;
+		}
+	}
+
+	private void btnMinus_Clicked(object sender, EventArgs e)
+	{
+		if (noPerson > 1) { 
+			noPerson--;
+		}
+		lblNoPersons.Text = noPerson.ToString();
+		CalculateTotal();	
+	}
+
+	private void btnMax_Clicked(object sender, EventArgs e)
+	{
+		noPerson++;
+		lblNoPersons.Text = noPerson.ToString();
+		CalculateTotal();
+	}
+}
+
